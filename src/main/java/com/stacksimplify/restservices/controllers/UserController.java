e9c8c421d556d1764d6304c25eec590d881ccb2a
Osmary Ostos <osmary_ostos@hotmail.com>
2019-08-28 09:20:59 -0500

    JPA Rest Services Devleopmnet Completed

--- src/main/java/com/stacksimplify/restservices/controllers/UserController.java
+++ src/main/java/com/stacksimplify/restservices/controllers/UserController.java
@@ -3,13 +3,16 @@ package com.stacksimplify.restservices.controllers;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.stacksimplify.restservices.entities.User;
@@ -17,14 +20,18 @@ import com.stacksimplify.restservices.services.UserService;
 
 //Controller -
 @RestController
+@RequestMapping("/users")
 public class UserController {
 	
 	//Autowire the UserService
-	@Autowired
-	private UserService userService;
+	private final UserService userService;
+	
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
 	
 	//getAllUsers Method
-	@GetMapping("/users")
+	@GetMapping
 	public List<User> getAllUsers() {
 		
 		return userService.getAllUsers();
@@ -34,28 +41,28 @@ public class UserController {
 	//Create User Method
 	//@RequestBody Annotation
 	//@PostMapping Annotation
-	@PostMapping("/users")
+	@PostMapping
 	public User createUser(@RequestBody User user) {
 		return userService.createUser(user);
 	}
 	
 	
 	//getUserById
-	@GetMapping("/users/{id}")
+	@GetMapping("/{id}")
 	public Optional<User> getUserById(@PathVariable("id") Long id){
 		return userService.getUserById(id);
 		
 	}
 	
 	//updateUserById
-	@PutMapping("/users/{id}")
+	@PutMapping("/{id}")
 	public User updateUserById(@PathVariable("id") Long id, @RequestBody User user) {
 		return userService.updateUserById(id, user);
 	}
 	
 	
 	//deleteUserById
-	@DeleteMapping("/users/{id}")
+	@DeleteMapping("/{id}")
 	public void deleteUserById(@PathVariable("id") Long id) {
 		userService.deleteUserById(id);
 	}
@@ -63,14 +70,17 @@ public class UserController {
 	
 	
 	//getUserByUsername
-	@GetMapping("/users/byusername/{username}")
+	@GetMapping("/byusername/{username}")
 	public User getUserByUsername(@PathVariable("username") String username) {
 		return userService.getUserByUsername(username);
 	}
 	
 	
 	
-	
+	@PostMapping("/saludar")
+	public ResponseEntity<String> holaOsmary(@RequestParam(name="nombre") String nombre){
+		return new ResponseEntity<>(String.format("Hola %s", nombre), HttpStatus.OK);
+	}
 	
 	
 	
