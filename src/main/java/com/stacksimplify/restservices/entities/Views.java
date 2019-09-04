d9825f2398959b8d61780deff311e6bec04302fd
Osmary Ostos <osmary_ostos@hotmail.com>
2019-09-04 10:04:08 -0500

    JsonView-FirstCommit

---
+++ src/main/java/com/stacksimplify/restservices/entities/Views.java
@@ -0,0 +1,16 @@
+package com.stacksimplify.restservices.entities;
+
+public class Views {
+	//extrenal class
+	public static class External{
+		
+	}
+	
+	
+	//intrenal class
+	public static class Internal extends External{
+		
+	}
+	
+
+}
